b53f7da077c67b79e7faf1d8c425b636d925888a
JJerBum <2hanjunbum6@gmail.com>
2023-10-30 16:05:27 +0900

    add: models & show todo model value

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+    "github.com/JJerBum/todo-app/models"
+
+    "fyne.io/fyne/v2"
     "fyne.io/fyne/v2/app"
     "fyne.io/fyne/v2/widget"
 )
 
 func main() {
+    // 새로운 앱 생성 
     a := app.New()
+
+    // TODO Application이 제목인 새로운 창을 만듭니다.
     w := a.NewWindow("TODO Application")
 
-    w.SetContent(widget.NewLabel("TODOs will go here"))
+
+    // window 크기를 300 by 400으로 조정 
+    w.Resize(fyne.NewSize(300, 400))
+
+    t := models.NewTodo("Show thie on the window")
+
+    // window에 content와 label을 지정해 줍니다.
+    w.SetContent(widget.NewLabel(t.String()))
+
+    // 보여줍니다.
     w.ShowAndRun()
+
+
 }
---
+++ models/todo.go
@@ -0,0 +1,21 @@
+package models
+
+import "fmt"
+
+type Todo struct {
+    Description string
+    Done bool
+}
+
+// NewTodo 함수는 Todo 구조체의 생성자입니다.
+// Done은 기본적으로 false로 처리 됩니다.
+func NewTodo(description string) Todo {
+    return Todo{description, false}
+}
+
+// String() 메서드는 Stringer 인터페이스를 구현한 메서드 입니다.
+// 이 메서드는 Todo구조체 값의 Description - Done형태로 값을 반환합니다.
+func (t Todo) String() string {
+    return fmt.Sprintf("%s - %t", t.Description, t.Done)
+}
+
