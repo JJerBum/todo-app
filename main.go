7eeb3c9a839c40ab24135ff374f59e6b0d5e08b2
JJerBum <2hanjunbum6@gmail.com>
2023-10-31 16:00:15 +0900

    feat: todo list

--- main.go
+++ main.go
@@ -1,31 +1,35 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/JJerBum/todo-app/models"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
+	"github.com/JJerBum/todo-app/models"
 )
 
 func main() {
-	// 새로운 앱 생성
 	a := app.New()
-
-	// TODO Application이 제목인 새로운 창을 만듭니다.
-	w := a.NewWindow("TODO Application")
-
-	// window 크기를 300 by 400으로 조정
+	w := a.NewWindow("TODO App")
 	w.Resize(fyne.NewSize(300, 400))
 
-	t := models.NewTodo("Show thie on the window")
-
+	data := []models.Todo{
+		models.NewTodo("Some stuff"),
+		models.NewTodo("Some more stuff"),
+		models.NewTodo("Some other things"),
+	}
+	todos := binding.NewUntypedList()
+	for _, t := range data {
+		todos.Append(t)
+	}
 	newtodoDescTxt := widget.NewEntry()
 	newtodoDescTxt.PlaceHolder = "New Todo Description..."
-	addBtn := widget.NewButton("Add", func() { fmt.Println("add buttom was cliked") })
+
+	addBtn := widget.NewButton("Add", func() {
+		todos.Append(models.NewTodo(newtodoDescTxt.Text))
+		newtodoDescTxt.Text = ""
+	})
 	addBtn.Disable()
 
 	newtodoDescTxt.OnChanged = func(s string) {
@@ -34,14 +38,11 @@ func main() {
 		if len(s) >= 3 {
 			addBtn.Enable()
 		}
-
-		fmt.Println("hi")
 	}
 
-	// window에 content와 label을 지정해 줍니다.
 	w.SetContent(
 		container.NewBorder(
-			nil, // 컨테이너 상단
+			nil, // TOP of the container
 
 			container.NewBorder(
 				nil, // TOP
@@ -52,18 +53,40 @@ func main() {
 				// take the rest of the space
 				newtodoDescTxt,
 			),
-
 			nil, // Right
 			nil, // Left
-
-			// 나머지는 나머지 공간을 모두 차지합니다.
-			container.NewCenter(
-				widget.NewLabel(t.String()),
-			),
+			// the rest will take all the rest of the space
+			widget.NewListWithData(
+				// the binding.List type
+				todos,
+				// func that returns the component structure of the List Item
+				// exactly the same as the Simple List
+				func() fyne.CanvasObject {
+					return container.NewBorder(
+						nil, nil, nil,
+						// left of the border
+						widget.NewCheck("", func(b bool) {}),
+						// takes the rest of the space
+						widget.NewLabel(""),
+					)
+				},
+				// func that is called for each item in the list and allows
+				// but this time we get the actual DataItem we need to cast
+				func(di binding.DataItem, o fyne.CanvasObject) {
+					ctr, _ := o.(*fyne.Container)
+					// ideally we should check `ok` for each one of those casting
+					// but we know that they are those types for sure
+					l := ctr.Objects[0].(*widget.Label)
+					c := ctr.Objects[1].(*widget.Check)
+					/*
+						diu, _ := di.(binding.Untyped).Get()
+						todo := diu.(models.Todo)
+					*/
+					todo := models.NewTodoFromDataItem(di)
+					l.SetText(todo.Description)
+					c.SetChecked(todo.Done)
+				}),
 		),
 	)
-
-	// 보여줍니다.
 	w.ShowAndRun()
-
 }
